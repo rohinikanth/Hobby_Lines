0cff28e507d1db14701432fcd598947a4c86e28d
Rohinikanth S <145427207+rohinikanth@users.noreply.github.com>
2025-06-13 20:25:55 +0530

    Update  Methods.py

---
+++ Python/Methods.py
@@ -0,0 +1,46 @@
+#List Methods
+
+l=[4,5,6,7,8,7]
+
+l.append(9)
+print("Append: ",l)
+
+l.extend([1,2])
+print("Extend the list: ",l)
+
+l.insert(2,1)
+print("insert at index 2: ",l)
+
+l.remove(1)
+print("Remove: ",l)
+
+l.pop()
+print("POPED: ",l)
+
+l.reverse()
+print("Reversed list: ",l)
+
+l.sort()
+print("Sorted: ", l)
+
+
+print("The index of elemnt 6: ",l.index(6))
+print("Count 7: ", l.count(7))
+
+l.clear()
+print(l)
+
+
+'''
+Output LIST methods:
+Append:  [4, 5, 6, 7, 8, 7, 9]
+Extend the list:  [4, 5, 6, 7, 8, 7, 9, 1, 2]
+insert at index 2:  [4, 5, 1, 6, 7, 8, 7, 9, 1, 2]
+Remove:  [4, 5, 6, 7, 8, 7, 9, 1, 2]
+POPED:  [4, 5, 6, 7, 8, 7, 9, 1]
+Reversed list:  [1, 9, 7, 8, 7, 6, 5, 4]
+Sorted:  [1, 4, 5, 6, 7, 7, 8, 9]
+The index of elemnt 6:  3
+Count 7:  2
+[]
+'''
