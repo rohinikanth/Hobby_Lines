d4e22c16b6b4ae022fca462574875e9b6bc148b7
Rohinikanth S <145427207+rohinikanth@users.noreply.github.com>
2026-02-23 19:47:36 +0530

    Update server.py

--- Coursersa_Network_Projects/EmotionDetector/server.py
+++ Coursersa_Network_Projects/EmotionDetector/server.py
@@ -1,36 +1,41 @@
-''' Executing this function initiates the application of sentiment
-    analysis to be executed over the Flask channel and deployed on
-    localhost:5000.
+'''Deploy a Flask application that will allow a user to provide
+a text string which will then be analyzed to determine which emotion amongst a set of 5
+is the most likely emotion being conveyed by the given text.
 '''
-from flask import Flask, render_template, request 
-from SentimentAnalysis.sentiment_analysis import sentiment_analyzer 
+from flask import Flask, request, render_template
+from EmotionDetection.emotion_detection import emotion_detector
 
-app =Flask(__name__)
+app = Flask("Emotion Detector")
 
-@app.route("/sentimentAnalyzer")
-def sent_analyzer():
-    ''' This code receives the text from the HTML interface and 
-        runs sentiment analysis over it using sentiment_analysis()
-        function. The output returned shows the label and its confidence 
-        score for the provided text.
+@app.route("/emotionDetector")
+def emotion_analyzer():
+    '''Retrieve the provided text string from the user, then pass the text
+    to be analyzed by the emotion detector. Finally, return a response displaying
+    the confidence scores across all emotions and the dominant emotion.
     '''
-    text_to_analyze = request.args.get('textToAnalyze')
-    response = sentiment_analyzer(text_to_analyze)
-    label =response['label']
-    score =response['score']
-    if label is None:
-        return "Invalid input! Try again."
-    else:
-        return "The given text has been identified as {} with a score of{}.".format(label.split('_')[1], score)
+    text_to_analyse = request.args.get('textToAnalyze')
+    emotion_result = emotion_detector(text_to_analyse)
+    anger = emotion_result['anger']
+    disgust = emotion_result['disgust']
+    fear = emotion_result['fear']
+    joy = emotion_result['joy']
+    sadness = emotion_result['sadness']
+    dominant_emotion = emotion_result['dominant_emotion']
+
+    if dominant_emotion is None:
+        return "Invalid text! Please try again"
+
+    response_str = f"""For the given statement, the system response is
+    'anger': {anger}, 'disgust': {disgust}, 'fear': {fear}, 'joy': {joy}, 'sadness': {sadness}.
+    The dominant emotion is <strong>{dominant_emotion}</strong>."""
+    return response_str
 
 @app.route("/")
 def render_index_page():
-    ''' This function initiates the rendering of the main application
-        page over the Flask channel
+    '''Render the index page to the user, this is where the text string to be
+    analyzed is provided and a response is displayed back to the user.
     '''
     return render_template('index.html')
 
 if __name__ == "__main__":
-    ''' This functions executes the flask app and deploys it on localhost:5000
-    '''
-    app.run(host = "0.0.0.0",debug =True,port=5000)
+    app.run(host="0.0.0.0", port=5000)
